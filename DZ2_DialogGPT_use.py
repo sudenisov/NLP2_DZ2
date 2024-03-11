ce1e35c23fd179f1166cf767356e35e777196dd4
sdenisov <sdenisov@bk.ru>
2024-03-11 20:34:19 +0300

    update

--- DZ2_DialogGPT_use.py
+++ DZ2_DialogGPT_use.py
@@ -6,9 +6,17 @@ title = "DZ2 DialogGPT"
 description = "DialogGPT"
 examples = [["Hello?"]]
 
+stopping_criteria = {
+    "max_length": 100,
+    "num_return_sequences": 1,
+    "do_sample": True,
+    "top_k": 50
+}
+
 class DialogGPTWrapper:
-    def __init__(self, model_path="./DZ2model/DialogGPT_2"):
-        self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+    def __init__(self, model_path="./DZ2model/DialogGPT_2_ver2"):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path, padding_side='left'
+                                                       )
         self.model = AutoModelForCausalLM.from_pretrained(model_path)
 
     def predict(self, input, history=[]):
@@ -22,11 +30,12 @@ class DialogGPTWrapper:
 
         # генерирую ответы
         history = self.model.generate(
-            bot_input_ids, max_length=4000, pad_token_id=self.tokenizer.eos_token_id
+            bot_input_ids,do_sample=True, num_return_sequences=1
         ).tolist()
 
         # преобразовываю токены в текст, а затем разделяю ответы на строки
-        response = self.tokenizer.decode(history[0]).split("<|endoftext|>")
+        response = self.tokenizer.decode(history[0]
+                                         ).split("<|endoftext|>")
         # преобразовываю в кортежи из списка
         response = [(response[i], response[i + 1]) for i in range(0, len(response) - 1, 2)]
         return response, history
